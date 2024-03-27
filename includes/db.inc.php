778141c579acb76df9103aa8048dd068b1e410b3
Elaine <elainelozanobelmonte@gmail.com>
2024-03-27 13:47:30 +0800

    Update db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -5,9 +5,12 @@ $dbname = "arms";
 $dbusername = "root";
 $dbpassword = "";
 
-try {
-  $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+//Create Connection
+$conn = new mysqli ($dbhost, $dbusername, $dbpassword, $dbname);
+
+//Check Connection
+if ($conn->connect_error) {
+  die("connection failed." . $conn->connect_error);
 }
+
+?>
